4587225763f03d1ba15212bd66528f2689d047c1
IonutGaina <gainaionutz@gmail.com>
2022-11-22 14:03:15 +0200

    found the err segment

--- skel-lin/loader/loader.c
+++ skel-lin/loader/loader.c
@@ -14,24 +14,43 @@
 
 #include "exec_parser.h"
 
+#define PAGESIZE getpagesize() //PAGESIZE is 4096
 static so_exec_t *exec;
 
 static void segv_handler(int signum, siginfo_t *info, void *context)
 {
-	
 
-	printf("address fault - %p\n", info->si_addr);
-	printf("signum - %d\n", signum);
-	printf("context - %p\n", context);
+	int segment_no = -1;
+	so_seg_t *seg_err = NULL;
 
-	printf("\nexec struct\n\n");
-
-	printf("base_addr: %p\n", exec->base_addr);
-	printf("entry - %p\n", exec->entry);
+	// printf("base_addr: %d\n", exec->base_addr);
+	// printf("entry - %d\n", exec->entry);
 	printf("segments_no - %d\n", exec->segments_no);
+	printf("address fault - %d\n", (uintptr_t)info->si_addr);
+	// printf("signum - %d\n", signum);
+	// printf("context - %p\n", context);
 
-	/* TODO - actual loader implementation */
+	uintptr_t fault_addr = (uintptr_t)info->si_addr;
+	for( int i = 0; i < exec->segments_no ; i++ ) {
+		// printf("segment no %d with addr %d \n", i , exec->segments[i].vaddr);
+		uintptr_t seg_vaddr = exec->segments[i].vaddr;
+		if ( (int)(seg_vaddr - fault_addr) <= 0){
+			seg_err = &exec->segments[i];
+			segment_no = i;
+			break;
+		}
+	}
+
+	if (seg_err == NULL) {
+		printf("Don't exist segment");
+		exit(0);
+	}
+
+	printf("segment number - %d \n", segment_no);
+	printf("segment start - %p\n", (void*)seg_err->vaddr);
+
 	exit(0);
+	/* TODO - actual loader implementation */
 }
 
 int so_init_loader(void)
